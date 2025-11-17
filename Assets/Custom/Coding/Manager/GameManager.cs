31613a250e185338a4d2e09a72030c68c4a27944
athirujpao <165152150+Athir0j@users.noreply.github.com>
2025-11-17 14:03:39 +0700

    Main stuct complete
    หรือนำไปใช้จริงชึ่งขอรอตอน merge กันรอบแรกก่อน ถ้าไม่มีไร จะได้ นำไปใช้งาน ใน player กับ baseAI

--- Assets/Custom/Coding/Manager/GameManager.cs
+++ Assets/Custom/Coding/Manager/GameManager.cs
@@ -1,11 +1,32 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    public struct WorldTierStats
+    {
+        public float enemyHealthBuff, enemyDamageBuff, playerDamageNerf;
+        public int extraMonsterCap;
+        public int worldTier;
+        public int extraMon;
+    }
     private static GameManager instance;
     [SerializeField]private UIManager UIManager;
-    public int worldTier;
+    
 
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            //DontDestroyOnLoad(gameObject); // (ถ้า GameManager ต้องอยู่ตลอด)
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public static GameManager GetInstance()
     {
         return instance;
@@ -14,7 +35,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        worldTier = 1;
+        extraMon = 0;
     }
 
     // Update is called once per frame
@@ -25,6 +47,17 @@ public class GameManager : MonoBehaviour
 
     public void CheckCompleteState()
     {
-        
+       
+        bool isStateComplete = true; //สมมติว่าเช็คแล้วว่าผ่าน
+
+        if (isStateComplete)
+        {
+            worldTier++;
+            if (StatusController.Instance != null)
+            {
+                StatusController.Instance.IncreaseWorldTier();
+            }
+            
+        }
     }
 }
--- Assets/Custom/Coding/Manager/StatusController.cs
+++ Assets/Custom/Coding/Manager/StatusController.cs
@@ -1,12 +1,95 @@
 using UnityEngine;
+using System.Collections.Generic;
+
 
 public class StatusController : MonoBehaviour
 {
-    public Enemies Enemies;
-    public Player player;
+    
+    public static StatusController Instance { get; private set; }
 
-    public void IncreaseWorldTier()
+    public int CurrentWorldTier { get; private set; } = 1;
+    public GameManager.WorldTierStats CurrentStats { get; private set; }
+    
+    private Dictionary<int, WorldTierStats> tierStatsData;
+
+    void Awake()
     {
         
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            InitializeTierData();
+            
+            //tier 1 ก่อนเลย
+            ApplyCurrentTierSettings(true);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    private void InitializeTierData()
+    {
+        tierStatsData = new Dictionary<int, WorldTierStats>();
+
+        
+        tierStatsData[1] = new WorldTierStats {
+            enemyHealthBuff = 1.0f,
+            enemyDamageBuff = 1.0f,
+            playerDamageNerf = 1.0f,
+            extraMonsterCap = 0
+        };
+
+        
+        tierStatsData[2] = new WorldTierStats {
+            enemyHealthBuff = 1.3f,  
+            enemyDamageBuff = 1.15f, 
+            playerDamageNerf = 0.9f, 
+            extraMonsterCap = 3      
+        };
+
+        
+        tierStatsData[3] = new WorldTierStats {
+            enemyHealthBuff = 1.6f,  
+            enemyDamageBuff = 1.3f,  
+            playerDamageNerf = 0.8f, 
+            extraMonsterCap = 5      
+        };
+        
+        
+    }
+
+    //GameManagerจะเรียกใช้
+    public void IncreaseWorldTier()
+    {
+        int nextTier = CurrentWorldTier + 1;
+        if (tierStatsData.ContainsKey(nextTier))
+        {
+            CurrentWorldTier = nextTier;
+            ApplyCurrentTierSettings(false); //เรียกใช้ค่า Tier ใหม่
+            Debug.Log($"World Tier Increased to: {CurrentWorldTier}");
+        }
+        else
+        {
+            Debug.Log("Max World Tier reached!");
+        }
+    }
+
+    
+    private void ApplyCurrentTierSettings(bool isInitialization)
+    {
+        if (tierStatsData.TryGetValue(CurrentWorldTier, out GameManager.WorldTierStats settings))
+        {
+            CurrentStats = settings;
+            
+        }
     }
 }
